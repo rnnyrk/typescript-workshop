3e1daa458c412cea50bed2532224bda4ae65e1ce
Ronny <ronny.rook@labela.nl>
2019-10-09 16:28:45 +0200

    Remove boilerplate code

--- src/store.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import * as appReducers from 'ducks';
-
-const middleware = applyMiddleware(thunk);
-const reducers = combineReducers({ ...appReducers });
-
-export default createStore(reducers, middleware);
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,3 @@ export * from './general';
 export * from './redux';
 
 export * from 'styles/types';
-
-export * from 'ducks/types';
