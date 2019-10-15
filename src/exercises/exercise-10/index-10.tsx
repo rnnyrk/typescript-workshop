9737b100b3b940e69f3be219b7122c22a429b9a8
Sander Vispoel <sander.vispoel@labela.nl>
2019-10-15 13:46:57 +0200

    Add test to exercise 10

--- src/exercises/exercise-10/index-10.tsx
+++ src/exercises/exercise-10/index-10.tsx
@@ -1,50 +1,66 @@
+/*
+ * ======================================================
+ * Exercise 10
+ * Typing Redux ducks
+ *
+ * TO RUN TEST
+ * npm run test-10
+ * ======================================================*/
+
+
+
+
 import { action, ActionType } from 'typesafe-actions';
 
-export default () => {
-  /*
-  * ======================================================
-  * TODO: This is a (somewhat) our basic duck file
-  *
-  * 1. Update the actions with typed parameters that passes payload / error
-  * 2. Add user actions to ActionType (tip: Look into TypeScript typeof)
-  * 3. Add the missing actions in the reducer
-  * ======================================================*/
-  type BaseState<DataType> = {
-    loading: boolean;
-    error?: string;
-    data: DataType;
+/*
+* ======================================================
+* TODO: This is a simplified version of our basic duck file
+*
+* 1. Update the actions with typed parameters that passes payload / error
+* 2. Add a return type to reducer so it can never return anything else than UserState
+* 3. Add user actions to ActionType (tip: Look into TypeScript 'typeof')
+* 4. Add the missing actions in the reducer
+* ======================================================*/
+export type BaseState<DataType> = {
+  loading: boolean;
+  error?: string;
+  data: DataType;
+};
+
+export type UserState = BaseState<{
+  name: string;
+}>;
+
+const initalState: UserState = {
+  loading: false,
+  error: undefined,
+  data: {
+    name: 'Sander',
+  },
+};
+
+// 1. Update the missing actions with typed parameters
+export const userActions = {
+  getUser: () => action('user/GET'),
+
+  // 'user/GET_SUCCESS' with payload
+  getUserSuccess: () => {},
+
+  // 'user/GET_FAILED' with error
+  getUserFailed: () => {},
+};
+
+// 2. Add a return type to reducer so it can never return anything else than UserState
+// 3. Add user actions to ActionType(tip: Look into TypeScript typeof)
+export const reducer = (state = initalState, action: ActionType<>) => {
+  switch (action.type) {
+    case 'user/GET':
+      return {
+        ...state,
+        loading: true,
+      };
+    // 4. Add the missing actions in the reducer
+    default:
+      return state;
   }
-
-  type UserState = BaseState<{
-    name: string;
-  }>;
-
-  const initalState: UserState = {
-    loading: false,
-    error: undefined,
-    data: {
-      name: 'Sander',
-    },
-  };
-
-  // 1. Update the actions with typed parameters that passes payload / error
-  const userActions = {
-    getUser: () => action('user/GET'),
-    getUserSuccess: () => { },
-    getUserFailed: () => { },
-  };
-
-  // 2. Add user actions to ActionType(tip: Look into TypeScript typeof)
-  const reducer = (state = initalState, action: ActionType<>) => {
-    switch (action.type) {
-      case 'user/GET':
-        return {
-          ...state,
-          loading: true,
-        };
-      // 3. Add the missing actions in the reducer
-      default:
-        return state;
-    }
-  };
 };
